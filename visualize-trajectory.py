fa966db6634fb90d1734c1dcf56bbe46892125ab
SiddhartaDutta <siddhartadutta99@gmail.com>
2025-03-29 16:43:38 -0700

    Added plt test + gitignore

---
+++ visualize-trajectory.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 1, 3, 5]
+
+# Create the plot
+plt.plot(x, y)
+
+# Add labels and title
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Simple Line Plot")
+
+# Show the plot
+plt.show()
