d6257b904de7479a1980229a94d51550f7fbdb91
SiddhartaDutta <siddhartadutta99@gmail.com>
2025-04-07 16:18:26 -0700

    Added anti-race file generation

--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -2,6 +2,43 @@
 
 #include <iostream>
 #include <fstream>
+#include <filesystem>
+#include <algorithm>
+// #include <iterator>
+#include <thread>
+
+namespace fs = std::filesystem;
+
+string generate_data_file_path() {
+
+    // Locate data folder
+    fs::path target_path = fs::canonical(fs::read_symlink("/proc/self/exe"));
+    target_path = target_path.parent_path().parent_path().append("data");
+
+    // Count number of files
+    int count;
+    try {
+        count = std::count_if(
+            fs::directory_iterator(target_path),
+            fs::directory_iterator(),
+            [](const auto& entry) {
+                return entry.is_regular_file();
+            }
+        );
+    } catch (const fs::filesystem_error& e) {
+        std::cerr << "Error: " << e.what() << '\n';
+        return 0;
+    }
+
+    // Create file name
+    std::stringstream file;
+    file << std::to_string(count + 1) << "-" << std::this_thread::get_id() << ".csv";
+    string temp_file = file.str();
+
+    // Return full file path
+    return target_path.append(temp_file);
+    
+}
 
 // Constructor
 trajectory::trajectory():R(6371000), alt_interval(50), tangent_factor(10){
@@ -37,10 +74,11 @@ void trajectory::calculate_trajectory(){
     number_of_points = x_trajectory_values.size();
 
     // Store data
-    std::ofstream output_csv("t1.csv");
+    // Generate File
+    std::ofstream output_csv(generate_data_file_path());
 
     if (!output_csv.is_open()) {
-        std::cerr << "Error opening file!" << std::endl;
+        std::cerr << "Error generating file." << std::endl;
         return;
     }
 
