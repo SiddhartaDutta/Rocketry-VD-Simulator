df04ac495c643d5c28f8c2255391e1ab08908ad9
SiddhartaDutta <siddhartadutta99@gmail.com>
2025-04-16 04:12:25 -0700

    added manifest + data helpers for chosen data-gen architecture

--- header/misc.h
+++ header/misc.h
@@ -3,16 +3,18 @@
 
 #pragma once
 
+#include "json.hpp"
+
 #include <iostream>
 #include <filesystem>
 #include <algorithm>
 #include <thread>
 
-using namespace std;
+using json = nlohmann::json;
 namespace fs = std::filesystem;
 
 namespace misc {
-    string generate_data_file_path() {
+    std::string generate_data_file_path() {
 
         // Locate data folder
         fs::path target_path = fs::canonical(fs::read_symlink("/proc/self/exe"));
@@ -40,6 +42,45 @@ namespace misc {
     
         // Return full file path
         return target_path.append(temp_file);
+        
+    }
+
+    bool record_to_manifest(string output_csv, bool feasible, float downrange_dist){
+        // Locate manifest
+        fs::path target_path = fs::canonical(fs::read_symlink("/proc/self/exe"));
+        target_path = target_path.parent_path().parent_path().append("data/manifest.csv");
+
+        try{
+            std::ofstream manifest_csv(target_path);
+
+            // Store to manifest
+            manifest_csv << output_csv << ',' << feasible << ',' << downrange_dist << endl; 
+
+            return 0;
+        } catch (const std::runtime_error& error) {
+            std::cerr << "Error: " << error.what() << std::endl;
+            return 1; 
+        }
+
+    }
+
+    bool record_to_log_file(std::ofstream output_csv, vector<float> downrange_dist, vector<float> altitude, vector<float> vehicle_speed, vector<float> thrust){        
+
+        try {
+            // Write column titles
+            output_csv << "alititude, downrange_dist, vehicle_speed, thrust" << endl;
+
+            for(int i = 0; i < downrange_dist.size(); i++){
+                output_csv << altitude[i] << ',' << downrange_dist[i] << ',' << vehicle_speed[i] << ',' << thrust[i] << endl;
+            }
+
+            // Write data
+            // output_csv << 
+            return 1;
+        }
+        
+
+    } catch (){
         
     }
     
