bdf0fa6f2b113d5f54d127354be0c60cbe55424e
SiddhartaDutta <siddhartadutta99@gmail.com>
2025-07-20 19:15:26 -0700

    Added more member methods needed for sim

--- python implementation/rocket.py
+++ python implementation/rocket.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 
 class Rocket:
-    def __init__(self, json_path):
+    def __init__(self, altitude, json_path):
         with open(json_path) as file:
             json_data = json.load(file)
 
@@ -14,10 +14,38 @@ class Rocket:
         self.height = json_data['rocket']['dimensions']['height_m']
         self.diameter = json_data['rocket']['dimensions']['diameter_m']
         self.drag_coeff = json_data['rocket']['dimensions']['drag_coeff']
+        self.dry_weight = json_data['rocket']['dimensions']['dry_weight']
 
         self.fuel_density = json_data['rocket']['fuel']['fuel_density']
         self.OtF_ratio = json_data['rocket']['fuel']['OtF_ratio']
         self.ox_density = json_data['rocket']['fuel']['ox_density']
+
+        self.altitude = altitude
+        self.acceleration = 0.0
+        self.velocity = 0.0
+        self.thrust = 0.0
+
+        self.fuel = 0.0
+        self.oxidizer = 0.0
+        self.burnrate = 0.0
+        self.total_fuel_mass = (self.fuel * self.fuel_density) + (self.fuel * self.OtF_ratio * self.ox_density)
+
+    def get_state(self):
+        return self.altitude, self.acceleration, self.velocity
+
+    def gravity_at_altitude(self) -> float:
+        g0 = 9.80665
+        R = 6.371e6
+        return g0 * (R / (R + self.altitude)) ** 2
+    
+    def air_density_at_altitude(self) -> float:
+        pass
+
+    def dynamic_pressure(self) -> float:
+        pass
+
+    def step(self):
+        pass
 
     def print(self):
         print(self.ox_density)
